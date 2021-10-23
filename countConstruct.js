9990171dbfd8498fb84c2fddac56df4456a4413e
Anthony Sim <anthonyysim@gmail.com>
2021-10-23 12:22:43 -0700

    countConstruct

--- countConstruct.js
+++ countConstruct.js
@@ -1,18 +1,17 @@
 function countConstruct(target, wordDict, memo = {}) {
   if (target in memo) return memo[target];
-  if (target === "") return true;
+  if (target === "") return 1;
+
+  let totalCount = 0;
 
   for (let word of wordDict) {
     if (target.indexOf(word) === 0) {
-      let suffix = target.slice(word.length);
-      if (countConstruct(suffix, wordDict, memo)) {
-        memo[target] = true;
-        return true;
-      }
+      let numWays = countConstruct(target.slice(word.length), wordDict, memo);
+      totalCount += numWays;
     }
   }
-  memo[target] = false;
-  return false;
+  memo[target] = totalCount;
+  return memo[target];
 };
 
 console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])); // true
